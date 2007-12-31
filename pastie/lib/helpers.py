c4bae71e2f2f9b268d19d78369471f062f42b181
Pedro Algarvio <ufs@ufsoft.org>
2007-12-31 00:08:13 +0000

    [svn r43] Made minifying the JS files automatic upon the first request to them.

--- pastie/lib/helpers.py
+++ pastie/lib/helpers.py
@@ -5,3 +5,35 @@ available to Controllers. This module is available to both as 'h'.
 """
 from pylonsgenshi.helpers import *
 from pastie.lib.highlight import *
+from pylons import config
+import os
+import logging
+
+log = logging.getLogger(__name__)
+
+from webhelpers.rails.asset_tag import compute_public_path, javascript_builtins
+
+__javascript_include_tag = javascript_include_tag
+
+def javascript_include_tag(*sources, **options):
+    if options.pop('minified', False):
+        from pastie.jsmin import JavascriptMinify
+        jsm = JavascriptMinify()
+        if not config.get('debug', False):
+            root = config.get('pylons.paths').get('static_files')
+            _sources = []
+            for source in sources:
+                _source = os.path.join(root, *(source[:-3]+'.min.js').split('/'))
+                if os.path.exists(_source):
+                    _sources.append(source[:-3]+'.min.js')
+                else:
+                    _source = os.path.join(root, *source.split('/'))
+                    minified = _source[:-3]+'.min.js'
+                    log.warning('minifying %s -> %s', source,
+                                source[:-3]+'.min.js')
+                    jsm.minify(open(_source, 'r'), open(minified, 'w'))
+                    _sources.append(source[:-3]+'.min.js')
+            sources = _sources
+    return __javascript_include_tag(*sources, **options)
+
+
