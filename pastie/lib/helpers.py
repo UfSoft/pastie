e34b6bac212f6b33ee41c63b015b5e70033e21f5
Pedro Algarvio <ufs@ufsoft.org>
2007-12-31 16:23:39 +0000

    [svn r44] Cache into dbm the "minification" of javascripts which will never expire. Base JS libs called in a sible time, no need to call `h.javascript_include_tag` twice.

--- pastie/lib/helpers.py
+++ pastie/lib/helpers.py
@@ -8,32 +8,38 @@ from pastie.lib.highlight import *
 from pylons import config
 import os
 import logging
+from pylons.decorators.cache import beaker_cache
+
 
 log = logging.getLogger(__name__)
 
-from webhelpers.rails.asset_tag import compute_public_path, javascript_builtins
-
 __javascript_include_tag = javascript_include_tag
 
+
 def javascript_include_tag(*sources, **options):
-    if options.pop('minified', False):
-        from pastie.jsmin import JavascriptMinify
+    @beaker_cache(key='sources', expire='never', type='dbm')
+    def get_sources(sources):
+        log.debug('Generating minified sources if needed')
+        from pastie.lib.jsmin import JavascriptMinify
         jsm = JavascriptMinify()
+        _sources = []
+        root = config.get('pylons.paths').get('static_files')
+        for source in sources:
+            _source = os.path.join(root, *(source[:-3]+'.min.js').split('/'))
+            if os.path.exists(_source):
+                _sources.append(source[:-3]+'.min.js')
+            else:
+                _source = os.path.join(root, *source.split('/'))
+                minified = _source[:-3]+'.min.js'
+                log.debug('minifying %s -> %s', source,
+                            source[:-3]+'.min.js')
+                jsm.minify(open(_source, 'r'), open(minified, 'w'))
+                _sources.append(source[:-3]+'.min.js')
+        return _sources
+
+    if options.pop('minified', False):
         if not config.get('debug', False):
-            root = config.get('pylons.paths').get('static_files')
-            _sources = []
-            for source in sources:
-                _source = os.path.join(root, *(source[:-3]+'.min.js').split('/'))
-                if os.path.exists(_source):
-                    _sources.append(source[:-3]+'.min.js')
-                else:
-                    _source = os.path.join(root, *source.split('/'))
-                    minified = _source[:-3]+'.min.js'
-                    log.warning('minifying %s -> %s', source,
-                                source[:-3]+'.min.js')
-                    jsm.minify(open(_source, 'r'), open(minified, 'w'))
-                    _sources.append(source[:-3]+'.min.js')
-            sources = _sources
+            sources = get_sources([source for source in sources])
     return __javascript_include_tag(*sources, **options)
 
 
